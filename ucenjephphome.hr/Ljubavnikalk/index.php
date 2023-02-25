ea62278b341cd8147e4fc1b83c2bfcbadb847ba6
ChrisLovric <klovric991@gmail.com>
2023-02-25 19:39:37 +0100

    dodavanje foundationa

--- ucenjephphome.hr/Ljubavnikalk/index.php
+++ ucenjephphome.hr/Ljubavnikalk/index.php
@@ -26,31 +26,46 @@ if($_POST){
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html class="no-js" lang="en" dir="ltr">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ljubavni Kalkulator</title>
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/app.css">
 </head>
 <body>
-    <h1>Ljubavni kalkulator</h1>
+
+<div class="grid-container">
+<div class="grid-x grid-padding-x">
+    <div class="large-12 medium-8 small-6 cell">
+        <div class="row text-center">
+          <h1>Ljubavni kalkulator</h1>
+        </div>
     
-<div>
+      <div class="container">
+        <div class="row text-center">
+          <div class="large-8 cell">
     <form action="index.php" method="post">
         <label for="prvoime">Unesi prvo ime:</label><br>
         <input type="text" name="prvoime" value="<?=$ime1?>" placeholder="prvo ime" id="prvoime"><br><br>
         <label for="drugoime">Unesi drugo ime:</label><br>
-        <input type="text" name="drugoime" value="<?=$ime2?>" placeholder="drugo ime" ide="drugoime"><br><br><br>
-        <input type="submit" value="Izračunaj postotak zaljubljenosti">
+        <input type="text" name="drugoime" value="<?=$ime2?>" placeholder="drugo ime" id="drugoime"><br><br><br>
+        <input type="submit" value="Izračunaj postotak zaljubljenosti" id="postotak">
     </form><br>
-    <h1>
+    <h2>
         <?php
         if(isset($postotak)){
             echo $postotak;
         }
         ?>
-    </h1>
+    </h2>
+</div>
+</div>
+</div>
+</div>
+</div>
 </div>
 </body>
 </html>
