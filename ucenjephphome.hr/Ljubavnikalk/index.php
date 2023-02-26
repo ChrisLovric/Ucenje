993b809e72dfa42a06f6dcedb2dbf4aa1b4f844d
ChrisLovric <klovric991@gmail.com>
2023-02-26 13:00:48 +0100

    dodavanje error poruke putem varijable

--- ucenjephphome.hr/Ljubavnikalk/index.php
+++ ucenjephphome.hr/Ljubavnikalk/index.php
@@ -2,15 +2,17 @@
 
 require_once 'funkcija.php';
 
+$errorporuka='';
+
 if($_POST){
     $ime1=$_POST['prvoime'];
     $ime2=$_POST['drugoime'];
     if($ime1==='' && $ime2===''){
-        echo "OBAVEZAN UNOS OBA IMENA!";
-    }elseif($ime1==='' && $ime2!==''){
-        echo "OBAVEZAN UNOS OBA IMENA!";
-    }elseif($ime1!=='' && $ime2===''){
-        echo "OBAVEZAN UNOS OBA IMENA!";
+        $errorporuka='OBAVEZAN UNOS OBA IMENA!';
+    }else if($ime1==='' && $ime2!==''){
+        $errorporuka='OBAVEZAN UNOS OBA IMENA!';
+    }else if($ime1!=='' && $ime2===''){
+        $errorporuka='OBAVEZAN UNOS OBA IMENA!';
     }else{
     $ukupno=Zbroj($ime1,$ime2);
     $postotak=Postotak($ukupno);
@@ -19,6 +21,7 @@ if($_POST){
 }else{
     $ime1='';
     $ime2='';
+    $errorporuka='';
 }
 
 
@@ -39,14 +42,13 @@ if($_POST){
 
 <div class="grid-container">
 <div class="grid-x grid-padding-x">
-    <div class="large-12 medium-8 small-6 cell">
+    <div class="large-12 medium-6 small-6 cell">
         <div class="row text-center">
           <h1>Ljubavni kalkulator</h1>
         </div>
-    
+
       <div class="container">
         <div class="row text-center">
-          <div class="large-8 cell">
     <form action="index.php" method="post">
         <label for="prvoime">Unesi prvo ime:</label><br>
         <input type="text" name="prvoime" value="<?=$ime1?>" placeholder="prvo ime" id="prvoime"><br><br>
@@ -61,7 +63,7 @@ if($_POST){
         }
         ?>
     </h2>
-</div>
+    <h3><?=$errorporuka?></h3>
 </div>
 </div>
 </div>
