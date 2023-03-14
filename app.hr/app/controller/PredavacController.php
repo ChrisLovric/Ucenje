baf6e02a95efe7a67d5519d8c9cd5290b3778b23
ChrisLovric <klovric991@gmail.com>
2023-03-14 17:34:10 +0100

    predavanje 13.03.2023 - priprema, model i rad na predavacima

---
+++ app.hr/app/controller/PredavacController.php
@@ -0,0 +1,15 @@
+<?php
+
+class PredavacController extends AutorizacijaController
+{
+    private $viewPutanja='privatno' . DIRECTORY_SEPARATOR . 'predavaci' . DIRECTORY_SEPARATOR;
+    private $e;
+    private $poruka='';
+
+    public function index()
+    {
+        $this->view->render($this->viewPutanja . 'index',[
+            'podaci'=>Predavac::read()
+        ]);
+    }
+}
